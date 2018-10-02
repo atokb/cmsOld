05b5c1610f246118094b6c7fb4508ce06bf6ca55
atokb <ato15001@byui.edu>
2018-10-02 10:53:08 -0600

    in

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+import { ContactsDetailComponent } from './contacts/contacts-detail/contacts-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContactsComponent,
+    ContactListComponent,
+    ContactsDetailComponent
   ],
   imports: [
     BrowserModule
@@ -13,4 +21,20 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  public contactId: string;
+  public name: string;
+  public email: string;
+  public phone: string;
+  public imageUrl: string;
+  public group: string;
+
+  constructor(contactId: string, name: string, email: string, phone: string, imageUrl: string, group: string) {
+      this.contactId = contactId;
+      this.name = name;
+      this.email = email;
+      this.phone = phone;
+      this.imageUrl = imageUrl;
+      this.group = group;
+    }
+}
